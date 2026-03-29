2d633e1a8ad9e26947b097ded820f0d638e90f5e
dukono <devedo@outlook.com>
2026-03-29 17:15:33 +0200

    deploy

--- app/src/test/java/dukono/japp/adapter/in/rest/ItemControllerIT.java
+++ app/src/test/java/dukono/japp/adapter/in/rest/ItemControllerIT.java
@@ -1,17 +1,22 @@
 package dukono.japp.adapter.in.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
+@Slf4j
 class ItemControllerIT {
 
     @Autowired
@@ -22,6 +27,8 @@ class ItemControllerIT {
         String body = """
                 {"text": "Hola", "provider": "groq-llama"}
                 """;
+        log.info("body: {}", body);
+        System.out.println("body: "+ body);
         mockMvc.perform(post("/items")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(body)
